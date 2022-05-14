217e20433a06af206dc27f34e72a1c86a79c6a6b
Vil4ik <104685054+Vil4ik@users.noreply.github.com>
2022-05-14 16:46:15 +0300

    Window added.

---
+++ game.py
@@ -0,0 +1,18 @@
+from pygame import *
+
+
+window = display.set_mode((800, 500))
+background = transform.scale(
+    image.load('pin pong.jpg'),
+    (800, 500)
+)
+fps = 60
+clock = time.Clock()
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    window.blit(background, (0, 0))
+    display.update()
+    clock.tick(fps)
